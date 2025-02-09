2e1b8bf292f468de29c40db2ff1acf6180f56034
aryanoffical2706 <aryan.21ug2031@iiitranchi.ac.in>
2025-02-09 21:27:40 +0530

    fourth

--- app.py
+++
@@ -1,4 +0,0 @@
-print("Hello World!")
-print("New things are coming your ways!")
-print("New push")
-print("MEW MEW ")
---
+++ src/example.py
@@ -0,0 +1,14 @@
+def add_number(a,b):
+    "Function to add two numbers"
+    
+    return a+b
+
+
+def sub_number(a,b):
+    "Function to substract two numbers"
+    
+    return a-b
+
+
+print(add_number(1,2))
+print(sub_number(200,100))
