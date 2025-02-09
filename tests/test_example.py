f1563ca33d3821527c35d8ad1ee2024e88849448
aryanoffical2706 <aryan.21ug2031@iiitranchi.ac.in>
2025-02-10 01:03:42 +0530

    adding jenkins file

--- tests/test_example.py
+++ tests/test_example.py
@@ -2,17 +2,15 @@ import sys
 import os
 import unittest
 
-# Add the parent directory (project root) to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-# Now import from src
 from src.example import add_number, sub_number
 
 class Test_Example(unittest.TestCase):
     def test_add_number(self):
         self.assertEqual(add_number(100, 200), 300)
 
-    def test_sub_number(self):  # Fix duplicate function name
+    def test_sub_number(self): 
         self.assertEqual(sub_number(300, 200), 100)
 
 if __name__ == "__main__":
